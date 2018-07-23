53ef27cdd798e867159685f90531d32201c838b6
Ryan Ureta <rcureta95@gmail.com>
2018-07-23 11:25:09 -0700

    troubleshooting functionality

--- scripts/store.js
+++ scripts/store.js
@@ -9,8 +9,35 @@ const store = (function(){
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+
+  function findById(id){
+  	items.find(x => x === id);
+  }
   
+
+ function addItem(name){
+ 	try{Item.validateName(name)}
+	catch(e){console.error(`cannot add item: ${e}`)}
+	items.push(Item.create(name));
+ }
+ function findAndToggleChecked(id){
+	findById(id).checked = !findById(id).checked;
+ }
+
+ function findAndUpdateName(id,newName){
+	try{Item.validateName(name)}
+	catch(e){console.error(`cannot add items: ${e}`)}
+	findById(id).name = newName;
+ } 
+
+ function findAndDelete (id) {
+	const x = this.items.findIndex(findById(id));
+	this.items.splice(x,1);
+ }
+
   return {
-    items, hideCheckedItems, searchTerm
+    items, hideCheckedItems, searchTerm, findById, findAndDelete,findAndUpdateName,findAndToggleChecked, addItem
   };
 }() );
+
+
