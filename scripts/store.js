c49c5831b0f5dcc333fec1de7acca4d1ed6dfafd
Ryan Ureta <rcureta95@gmail.com>
2018-07-23 08:08:57 -0700

    step 1 troubleshooting

---
+++ scripts/store.js
@@ -0,0 +1,14 @@
+
+const store = (function(){
+  const items = [
+    const foo = 'bar';
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+  
+  return store:store;
+}() );
